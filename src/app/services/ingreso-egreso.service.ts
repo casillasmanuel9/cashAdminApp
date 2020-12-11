691c1b1006c54a2bf4bfc82601c038ed36c23887
Juan Manuel Galindo Casillas <casillasmanuel9@gmail.com>
2020-12-11 11:28:56 -0600

    servicio archivo faltante

---
+++ src/app/services/ingreso-egreso.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AuthService } from './auth.service';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngresoEgresoService {
+
+  constructor(
+    private fireStore: AngularFirestore,
+    private authService: AuthService
+  ) { }
+
+  crearIngresoEgreso(ingresoEgreso: any) {
+    delete ingresoEgreso.uid;
+    return this.fireStore
+      .doc(`${this.authService.user.uid}/ingresos-egresos`)
+      .collection('items')
+      .add({ ...ingresoEgreso });
+  }
+
+  initIngresosEgresosListener(uid: string) {
+    return this.fireStore
+      .collection(`${uid}/ingresos-egresos/items`)
+      .snapshotChanges()
+      .pipe(
+        map((snapshoot) =>
+          snapshoot.map((doc) => ({
+            uid: doc.payload.doc.id,
+            ...(doc.payload.doc.data() as any),
+          }))
+        )
+      );
+  }
+
+  borrarEgresoIngreso(uidItem: string) {
+    const uid = this.authService.user.uid;
+    return this.fireStore.doc(`${uid}/ingresos-egresos/items/${uidItem}`).delete();
+  }
+}
